511fed3abcef110ee153828e3809b54e658e3f46
kchiang <kaizhi3006@gmail.com>
2026-06-22 22:16:47 +0800

    feat: added staticfiles comment for future use in frontend app.py

--- frontend/src/app.py
+++ frontend/src/app.py
@@ -1,10 +1,12 @@
 from pathlib import Path
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from starlette.requests import Request
+# from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+# app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates_dir = Path(__file__).parent / "templates"
 templates = Jinja2Templates(directory=templates_dir)
